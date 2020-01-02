60f02a136046546acc1cf501d37572fd98a39916
robert-cummins <robert.k.cummins@gmail.com>
2020-01-02 17:03:46 +1300

    Add user auth checks

--- server/db/users.js
+++ server/db/users.js
@@ -1,1 +1,32 @@
-const connection = require('./connection')
+const database = require('./connection')
+
+const {generatePasswordHash} = require('../auth/hash')
+
+
+function createUser (email, first_name, last_name, password,testDb) {
+  const db = testDb || database
+
+  return generatePasswordHash(password)
+    .then(hash => {
+      return db('users').insert({email, first_name, last_name, hash}, "user_id")
+    })
+}
+
+function getUserByEmail(email, db = database){
+  return db('users').where('email', email).first()
+}
+
+function userExists (email, testDb) {
+  const db = testDb || database
+
+  return db('users')
+    .where('email', email)
+    .then(users => users.length > 0)
+}
+
+
+module.exports = {
+  userExists,
+  createUser,
+  getUserByEmail,
+}
