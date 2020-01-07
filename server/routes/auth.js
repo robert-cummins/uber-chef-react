5d9aca95bdf683e794ed3eec2a9bd3722fbc3fa1
robert-cummins <robert.k.cummins@gmail.com>
2020-01-07 15:14:46 +1300

    Add check user function to auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,26 @@ const router = require('express').Router()
 const { userExists, createUser } = require('../db/users')
 const token = require('../auth/token')
 
-// router.post('/register', register, token.issue)
+router.post('/login', token.issue)
+router.post('/register', register, token.issue)
+
+function register (req, res, next) {
+    const { email, password} = req.body
+    userExists(email)
+      .then(exists => {
+        if (exists) return res.status(400).send({ message: "Email In Use" })
+  
+        createUser(email, password)
+          .then(() => next())
+          .catch(err => {
+            console.log(err)
+            res.status(500).send({message: "Server Error"})
+          })
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).send({message: "Server Error"})
+      })
+  }
 
 module.exports = router
